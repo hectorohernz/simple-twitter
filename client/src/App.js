131cb6d900004009b20875fc7e0d67a80dd8ec6c
Hector Hernandez <hectorohernz@gmail.com>
2020-07-22 17:48:44 -0400

    heroku deployment

---
+++ client/src/App.js
@@ -0,0 +1,51 @@
+import React,{Component} from 'react'
+import Form from './components/Form';
+import axios from 'axios';
+import "./App.css";
+
+
+class App extends Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      tweet: null
+    };
+    this.fetchTweets = this.fetchTweets.bind(this);
+  }
+
+  componentDidMount() {
+    this.fetchTweets();
+  };
+  
+  async fetchTweets() {
+    const tweets = await axios.get('/api/feed');
+    this.setState({ tweet: tweets.data })
+  };
+
+  render() {
+    let tweet;
+  
+    if(this.state.tweet === null){
+      tweet = <h1>Loading..</h1>
+    } else{
+      tweet = <section>
+      {this.state.tweet.map(tweet =>
+        <div>
+          <h3>Comment: {tweet.tweet}</h3>
+          <p>Name: {tweet.name}</p>
+        </div>
+      )}
+       </section>
+    }
+
+    return (
+      <div className="main-container">
+        <h1>Simple Twitter</h1>
+        <Form/>
+        {tweet}
+      </div>
+    )
+  }
+}
+
+export default App;
