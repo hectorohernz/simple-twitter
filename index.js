9709889d2617efde338bad60384990c259fe8521
Hector Hernandez <hectorohernz@gmail.com>
2020-07-22 18:11:53 -0400

    adding connent

--- index.js
+++ index.js
@@ -4,9 +4,6 @@ const connectMongoDB = require('./config');
 
 connectMongoDB();
 app.use(express.json({ extented: false, limit: '50mb' }));
-app.get('/', (req,res) => {
-    res.send("Hello World")
-});
 
 app.use("/api/post", require("./route/post"));
 app.use("/api/feed", require('./route/feed'));
